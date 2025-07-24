d0eacddd7669a03bb44559c622b4e0e67516320b
New Born Media <endrit.c@newborn.media>
2025-07-25 01:33:06 +0200

    Adding configs to the element and its functionality

--- custom/plugins/SwagBasicExampleTheme/src/Resources/app/administration/src/module/sw-cms/elements/custom-banner-slider/config/index.js
+++ custom/plugins/SwagBasicExampleTheme/src/Resources/app/administration/src/module/sw-cms/elements/custom-banner-slider/config/index.js
@@ -6,19 +6,121 @@ const {Component, Mixin} = Shopware;
 Component.register('sw-cms-el-config-custom-banner-slider', {
     template,
 
-    inject: ['repositoryFactory'],
+    inject: ['repositoryFactory','mediaService'],
 
     mixins: [
         Mixin.getByName('cms-element')
     ],
 
+    data() {
+        return {
+            mediaModalIsOpen: false,
+            initialFolderId: null,
+            openedMediaModalIndex: null
+        };
+    },
+
     created() {
         this.createdComponent();
+    },
+
+    computed: {
+        mediaRepository() {
+            return this.repositoryFactory.create('media');
+        },
     },
 
     methods: {
         createdComponent() {
             this.initElementConfig('custom-banner-slider');
+        },
+
+        uploadTag(index) {
+            return `cms-element-media-config-${this.element.id}-${index}`;
+        },
+
+        fileAccept() {
+            return "image/*";
+        },
+
+        addNewElement() {
+            this.element.config.slides.value.push(this.getEmptyElement())
+        },
+
+        removeElement(index) {
+            this.element.config.slides.value.splice(index, 1);
+        },
+
+        getEmptyElement() {
+            return {
+                image: {
+                    source: 'static',
+                    value: null,
+                    entity: {
+                        name: 'media'
+                    }
+                },
+                title: {
+                    source: 'static',
+                    value: ''
+                },
+                text: {
+                    source: 'static',
+                    value: ''
+                },
+                activeBtn: {
+                    source: 'static',
+                    value: false
+                },
+                link: {
+                    source: 'static',
+                    value: null
+                },
+                nameBtn: {
+                    source: 'static',
+                    value: ''
+                },
+            };
+        },
+
+        onImageRemove(index) {
+            this.element.config.slides.value[index].image.value = null;
+
+            this.$emit('element-update', this.element);
+        },
+
+
+        onSelectionChanges(mediaEntity) {
+            this.element.config.slides.value[this.openedMediaModalIndex].image.value = mediaEntity[0];
+
+            this.$emit('element-update', this.element);
+        },
+
+        onCloseModal() {
+            this.mediaModalIsOpen = false;
+            this.openedMediaModalIndex = null;
+        },
+
+        async onImageUploadFinish({ targetId }, index) {
+            const mediaEntity = await this.mediaRepository.get(targetId, Shopware.Context.api);
+
+            this.element.config.slides.value[index].image.value = mediaEntity;
+
+            this.$emit('element-update', this.element);
+        },
+
+        previewSource(index) {
+            if (null !== this.element.config.slides.value[index].image
+                && null !== this.element.config.slides.value[index].image.value) {
+                return this.element.config.slides.value[index].image.value.id;
+            }
+
+            return null;
+        },
+
+        onOpenMediaModal(index) {
+            this.mediaModalIsOpen = true;
+            this.openedMediaModalIndex = index;
         }
     },
 });
---
+++ custom/plugins/SwagBasicExampleTheme/src/Resources/public/administration/assets/index-CLWuJBca.js
@@ -0,0 +1,2 @@
+const o='{% block sw_cms_sidebar_block_overview_category_options %} {% parent %} <option value="custom-option">Custom Option</option> {% endblock %}',t=Shopware.Component.wrapComponentConfig({template:o});export{t as default};
+//# sourceMappingURL=index-CLWuJBca.js.map
