bea01d8bd78faf4a209ef64fdf5eee7057508e98
New Born Media <endrit.c@newborn.media>
2025-07-25 09:30:27 +0200

    creating a block and calling the element in the block

---
+++ custom/plugins/SwagBasicExampleTheme/src/Resources/app/administration/src/module/sw-cms/blocks/custom-option/custom-banner-slider/component/index.js
@@ -0,0 +1,7 @@
+import template from './sw-cms-block-custom-banner-slider.html.twig';
+
+const {Component} = Shopware;
+
+Component.register('sw-cms-block-custom-banner-slider', {
+    template
+});
---
+++ custom/plugins/SwagBasicExampleTheme/src/Resources/app/administration/src/module/sw-cms/blocks/custom-option/custom-banner-slider/preview/index.js
@@ -0,0 +1,8 @@
+import template from './sw-cms-preview-custom-banner-slider.html.twig';
+import './sw-cms-preview-custom-banner-slider.scss';
+
+const {Component} = Shopware;
+
+Component.register('sw-cms-preview-custom-banner-slider', {
+    template
+});
